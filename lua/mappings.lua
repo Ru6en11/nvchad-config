941f972be0930547c8ef139440f7e2c20687e00c
Vladislav K <krasnoschek003@gmail.com>
2025-04-23 12:46:00 +1000

    Add mapping for code formatting

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,5 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map('n', '<leader>w', ":w<CR>")
+map('n', '<leader>df', function ()
+  require("conform").format({async = true, lsp_fallback = true})
+end, {desc = "Code formatting"})
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
